7fa7171adf4489d0ace5cc7094adf98540839980
code-with-zulquar <codewithzulquar@gmail.com>
2024-09-16 18:57:41 +0530

    update state

--- State/src/State.jsx
+++ State/src/State.jsx
@@ -1,33 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function State() {
-
   const [value, setValue] = useState(20);
   const [bool, setBool] = useState(true);
   const [obj, setObj] = useState({ name: "zulquar", age: 32 });
-  
+
+  const conditional = bool ? "bg-blue-400" : "bg-red-700";
+
   return (
-    <div className='m-10'>
-
-      <h1 className='text-xl'>The Value is : {value} </h1>
-      <button className='border-2 border-black rounded px-2' onClick={() => setValue((pre) => pre + 1)}>Get Value</button> 
+    <div className="m-10">
+      <h1 className="text-xl">The Value is : {value} </h1>
+      <button
+        className="border-2 border-black rounded px-2"
+        onClick={() => setValue((pre) => pre + 1)}
+      >
+        Get Value
+      </button>
       <hr />
-      
-      <h1 className='text-xl'>The Value is : {bool.toString()}</h1>
-      <button className='border-2 border-black rounded px-2' onClick={() => setBool(!bool)}> Get Value</button>
+
+      <h1 className="text-xl">The Value is : {bool.toString()}</h1>
+      <button
+        className={`border-2 border-black rounded px-2 ${conditional} text-neutral-50`}
+        onClick={() => setBool(!bool)}
+      >
+        {" "}
+        Get Value
+      </button>
       <hr />
 
-      <h1 className='text-xl'>Name : {obj.name} and Age : {obj.age}</h1>
-      <button className='border-2 border-black rounded px-2' onClick={()=> setObj({...obj , age:35})}>Update Details</button>
+      <h1 className="text-xl">
+        Name : {obj.name} and Age : {obj.age}
+      </h1>
+      <button
+        className="border-2 border-black rounded px-2"
+        onClick={() => setObj({ ...obj, age: 35 })}
+      >
+        Update Details
+      </button>
+      <br />
+      <hr />
+      <h1 className="text-xl">
+        Name : {obj.name} , Age : {obj.age} and Gender :{obj.gender}
+      </h1>
+      <button
+        className="border-2 border-black rounded px-2"
+        onClick={() => setObj({ ...obj, gender: male })}
+      >
+        Update Object
+      </button>
     </div>
-  )
+  );
 }
-
-
-
-
-
-
-
-
-
